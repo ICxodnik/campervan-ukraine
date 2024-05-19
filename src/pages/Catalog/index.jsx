4eefdb6eb4031020db1715626c86f9129897bd10
Natalia <icxodnik@gmail.com>
2024-05-19 02:06:14 +0100

    add page styles

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -1,9 +1,12 @@
 import CamperList from "../../components/CamperList";
+import Filter from "../../components/Filter";
 import css from "./index.module.css";
 
 export default function Catalog() {
   return (
-    <div>
+    <div className={css.page}>
+      <Filter />
+
       <CamperList />
     </div>
   );
