cdc41e3071478b01556c9ecfe501c741cee0a2dc
Natalia <icxodnik@gmail.com>
2024-05-20 19:03:26 +0100

    persist filters and favorites in localStorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,49 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import camperReducer from "./camperSlice";
 import filterReducer from "./filterSlice";
 
+const camperConfig = {
+  key: "favorites",
+  whitelist: ["favorite"],
+  storage,
+};
+
+const filterConfig = {
+  key: "filters",
+  storage,
+};
+
+const camperReducerP = persistReducer(camperConfig, camperReducer);
+const filterReducerP = persistReducer(filterConfig, filterReducer);
+
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    campers: camperReducer,
-    filter: filterReducer,
+    campers: camperReducerP,
+    filter: filterReducerP,
+  },
+
+  middleware: (getDefaultMiddleware) => {
+    // ignore serializable checks for redux-persist library
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
   },
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
