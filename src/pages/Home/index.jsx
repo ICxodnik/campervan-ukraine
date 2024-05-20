296694fbf86d656186409a1b48cedfb29c7761b7
Natalia <icxodnik@gmail.com>
2024-05-20 21:44:38 +0100

    add homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,24 @@
+import css from "./index.module.css";
+import { Link } from "react-router-dom";
+
 export default function Home(props) {
-  return <div>Home</div>;
+  return (
+    <div className={css.homePage}>
+      <div
+        className={css.imageWrapper}
+        style={{
+          backgroundImage: "url(" + process.env.PUBLIC_URL + "/hero.jpg" + ")",
+        }}
+      >
+        <div className={css.combineTitle}>
+          <span className={css.bigTitle}>Ukrainian</span>
+          <span className={css.title}>campervans</span>
+        </div>
+
+        <Link className="navLink" to="/catalog">
+          <span className={css.logo}> to find yours</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
