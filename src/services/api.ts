5fbbed42ace9bec05d64b27c6901ca5d7a203cb1
Natalia <icxodnik@gmail.com>
2024-05-20 03:54:49 +0100

    convert campers to internal DTO

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,5 +6,52 @@ const client = axios.create({
 });
 
 export async function fetchCampers(): Promise<Camper[]> {
-  return await client.get<[]>("/advert");
+  const resp = await client.get<[]>("/advert");
+
+  return resp.data.map((item: any) => {
+    return {
+      id: item._id,
+      name: item.name,
+      description: item.description,
+      price: item.price,
+      width: item.width,
+      length: item.length,
+      height: item.height,
+      form: item.form,
+      consumption: item.consumption,
+      location: item.location,
+      tank: item.tank,
+      rating: item.rating,
+      reviews: item.reviews.map((review: any) => {
+        return {
+          reviewer: review.reviewer_name,
+          rating: review.reviewer_rating,
+          comment: review.comment,
+        } satisfies Review;
+      }),
+      galleryUrls: item.gallery,
+
+      details: prepareDetails(item),
+    } satisfies Camper;
+  });
+}
+
+function prepareDetails(camper: any): CamperDetail[] {
+  const details = [
+    ["adults", camper.adults],
+    ["children", camper.children],
+    ["engine", camper.engine],
+    ["transmission", camper.transmission],
+  ].concat(Object.entries(camper.details));
+
+  return details.map(([id, value]) => {
+    return {
+      id,
+      value,
+    };
+  });
+}
+
+function capitalize(str: string) {
+  return str[0].toUpperCase() + str.substring(1);
 }
