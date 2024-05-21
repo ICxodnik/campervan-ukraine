894794e767d4ebd6845cf9875d6a85e14f1df540
Natalia <icxodnik@gmail.com>
2024-05-21 03:30:08 +0100

    add message component

--- src/components/CamperList/index.jsx
+++ src/components/CamperList/index.jsx
@@ -3,6 +3,7 @@ import CamperItem from "./Camper";
 import { useState } from "react";
 import { Modal } from "../Modal";
 import CamperItemDetails from "./CamperDetails";
+import { Message } from "../Message";
 
 export default function CamperList({ campers }) {
   const [showModalEl, setShowModalEl] = useState(null);
@@ -13,12 +14,18 @@ export default function CamperList({ campers }) {
 
   return (
     <>
-      <div className={css.list}>
-        {campers.map((el) => (
-          <CamperItem el={el} key={el.id} showModal={toggleModal} />
-        ))}
-      </div>
-
+      {campers.length > 0 ? (
+        <div className={css.list}>
+          {campers.map((el) => (
+            <CamperItem el={el} key={el.id} showModal={toggleModal} />
+          ))}
+        </div>
+      ) : (
+        <Message
+          level="info"
+          message="Campers by your filters aren't available"
+        />
+      )}
       {showModalEl && (
         <Modal onClose={() => toggleModal()}>
           <CamperItemDetails
